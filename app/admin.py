0c55133768e75685acecd13142bdccaa3573758e
Sang <Sangg2109@gmail.com>
2025-12-15 21:45:11 +0700

    thêm quản lý sách, quản lý người dùng và thêm thể loại sách

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Book, Reader
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'quantity', 'available_copies') 
+    search_fields = ('title', 'author', 'isbn') 
+    list_filter = ('category',) 
+
+@admin.register(Reader)
+class ReaderAdmin(admin.ModelAdmin):
+    list_display = ('library_card_id', 'fullname', 'phone', 'created_at') 
+    search_fields = ('fullname', 'library_card_id', 'phone')
+
+admin.site.register(Category)
