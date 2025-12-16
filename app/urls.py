e06fa7246106e5a68439e39cb5429ffbf323c06e
Vo Tuan Kiet <kiet2005vo@gmail.com>
2025-12-16 23:12:48 +0700

    url.py, views.py

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('', views.home, name='home'),
+    path('resoucre/', views.resource, name='resource'),
+    path('profile/', views.profile, name='profile'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'app/home.html')
 # Create your views here.
+
+def resource(request):
+    return render(request, 'app/resource.html')
+
+def profile(request):
+    return render(request, 'app/profile.html')
